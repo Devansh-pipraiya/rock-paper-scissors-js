245ca933c5020a6eedd57a53054c61da1ad1d36e
Devansh Linux <145137014+Devansh-pipraiya@users.noreply.github.com>
2026-02-01 20:55:37 +0530

    feat: add getBotChoice and getHumanChoice function
    - getBotChoice() generate random choice: rock, paper or scissors
    - getHumanChoice() takes input from user and evaluate valid choice

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("script link check")
+function getBotChoice(){         // func to generate random choices of rock, paper, scissors(RPS)
+ 
+    let choice = Math.floor(Math.random()*3);  // generates random number either 0, 1 or 2
+    // console.log(choice);
+    switch (choice){
+        case 0 : return "rock";
+        case 1 : return "paper";
+        case 2 : return "scissors";
+    }
+}
+console.log(getBotChoice());
+
+
+function getPlayerChoice(){      // func to get user choice of rock,paper,scissors and return a valid choice either rock, paper or scissors
+
+    let input = prompt("enter your choice either rock or paper or scissors");
+    choice = input.toLowerCase();    // takes input and converts to lowercase PapEr -> paper
+
+    switch (choice){
+        case "rock" : return "rock";
+        case "paper" : return "paper";
+        case "scissors" : return "scissors"
+        default: return "enter valid value"
+    }
+}
+console.log(getPlayerChoice());
